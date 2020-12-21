6daae15f931103ae3afbabfebfdb3d58e95c51d8
Tarik <tarikmerrylees@gmail.com>
2020-12-21 15:22:22 +0000

    players and matches tables migrated and relevant models created

--- backend/database/migrations/2020_12_21_151103_create_players_table.php
+++ backend/database/migrations/2020_12_21_151103_create_players_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePlayersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name", 100);
+            $table->integer("wins")->default(0);
+            $table->integer("losses")->default(0);
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('players');
     }
 }
