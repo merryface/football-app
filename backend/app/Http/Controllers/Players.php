c211a7dddad3325163b52e5bc0cf3a2f9e95d91b
Tarik <tarikmerrylees@gmail.com>
2020-12-22 08:50:47 +0000

    added Players controller methods

--- backend/app/Http/Controllers/Players.php
+++ backend/app/Http/Controllers/Players.php
@@ -8,8 +8,29 @@ use App\Models\Player;
 
 class Players extends Controller
 {
-    public function index() // show all friends
+    public function index() // show all players
     {
         return Player::all();
+    }
+
+    public function store(Player $request) // add new player
+    {
+        $data = $request->all();
+        $player = Player::create($data);
+
+        return $player;
+    }
+
+    public function destroy(Player $player) // delete player
+    {
+        $player->delete();
+        return response(null, 204);
+    }
+
+    public function update(Request $request, Player $player) //update player
+    {
+        $data = $request->all();
+        $player->fill($data)->save();
+        return $player;
     }
 }
