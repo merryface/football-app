e0b4053a4996790e1df22d951dda5c471791a924
Tarik <tarikmerrylees@gmail.com>
2020-12-23 11:09:57 +0000

    added all the API routes

--- football-backend/routes/api.php
+++ football-backend/routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Players
+Route::get('/players', [Players::class, "index"]); // return all players
+Route::post('/players', [Players::class, "store"]); // add a new player
+Route::delete('players/{player}', [Players::class, "destroy"]); // remove a player
+Route::patch('players/{player}', [Players::class, "update"]); // Update a player
+
+
+// Matches
+Route::get('/matches', [Matches::class, "index"]); // return all players
+Route::get('/matches/{match}', [Matches::class, "show"]); // return a specific player
+Route::post('/matches', [Matches::class, "store"]); // add a new player
+Route::delete('matches/{match}', [Matches::class, "destroy"]); // remove a player
+Route::patch('matches/{match}', [Matches::class, "update"]); // Update a player
